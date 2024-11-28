dff7fac2d87294a4d4626c8f7ab40a5f69c8ce63
BruceSmith1551 <brucesmith1551@gmail.com>
2024-11-27 23:55:01 -0900

    Main file, testing push

---
+++ project_gamebuoy.cpp
@@ -0,0 +1,12 @@
+// Bruce Smith
+// 2024-11-27
+//
+// Contains main
+
+#include <iostream> // For IO debugging
+#include <SFML/Graphics.hpp> // For window/rendering calls
+
+int main()
+{
+   return 0;
+}
